4762013d55cf9296ee675a85fc3cd1e350db9f43
Leomar Linhares <leomar_linhares@outlook.com>
2020-09-17 09:43:11 -0300

    Update script.js of exercise 4 in module 2
    Resolution of exercise 4 in module 2.

--- JavaScript/Modulo_02/ex04/script.js
+++ JavaScript/Modulo_02/ex04/script.js
@@ -1,10 +1,22 @@
 var nomes = ["Diego", "Gabriel", "Lucas"];
 
-var lista = document.querySelector('#lista');
-
 for (let index = 0; index < nomes.length; index++) {
-    var item = document.createElement('li');
-    item.textContent = nomes[index]
-    lista.appendChild(item);
+var item = document.createElement('li');
+item.textContent = nomes[index]
+lista.appendChild(item);
 }
 
+function adicionar() {
+    let addToList = document.querySelector('#campoDeEntrada');
+    let lista = document.querySelector('#lista');
+    lista.innerHTML = "";
+    nomes.push(addToList.value);
+
+    for (let index = 0; index < nomes.length; index++) {
+        var item = document.createElement('li');
+        item.textContent = nomes[index]
+        lista.appendChild(item);
+        }
+
+    addToList.value = "";
+}
